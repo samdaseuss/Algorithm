5a32d49ad320f551ffae727b4af657a8bc75d9b0
yellpall <ohj3423@naver.com>
2023-04-04 05:22:57 +0900

    Add bubble sort

---
+++ algorithm/sorting/bubble_sort.js
@@ -0,0 +1,51 @@
+// UNOPTIMIZED ver.
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+// function bubbleSort(arr){
+//     for(var i = arr.length; i > 0; i--){
+//       for(var j = 0; j < i - 1; j++){
+//         console.log(arr, arr[j], arr[j+1]);
+//         if(arr[j] > arr[j+1]){
+//           var temp = arr[j];
+//           arr[j] = arr[j+1];
+//           arr[j+1] = temp;         
+//         }
+//       }
+//     }
+//     return arr;
+// }
+  
+// ES2015 Version
+// function bubbleSort(arr) {
+//     const swap = (arr, idx1, idx2) => {
+//       [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//     };
+//     for (let i = arr.length; i > 0; i--) {
+//       for (let j = 0; j < i - 1; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//           swap(arr, j, j + 1);
+//         }
+//       }
+//     }
+//     return arr;
+// }
+
+// OPTIMIZED ver.
+// Optimized BubbleSort with noSwaps
+function bubbleSort(arr){
+    var noSwaps;
+    for(var i = arr.length; i > 0; i--){
+      noSwaps = true;
+      for(var j = 0; j < i - 1; j++){
+        if(arr[j] > arr[j+1]){
+          var temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          noSwaps = false;         
+        }
+      }
+      if(noSwaps) break;
+    }
+    return arr;
+  }
+console.log(bubbleSort([8,1,2,3,4,5,6,7]));
+console.log(bubbleSort([34,21,4,632,123]));
