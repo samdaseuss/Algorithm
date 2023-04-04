1cb7a95c3c577a1e2bb25e0922c3ad43af2861b2
yellpall <ohj3423@naver.com>
2023-04-05 07:17:17 +0900

    Add selection sort

---
+++ algorithm/sorting/selection_sort.js
@@ -0,0 +1,25 @@
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let lowest = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowest]) lowest = j
+        }
+        let temp = arr[i];
+        arr[i] = arr[lowest];
+        arr[lowest] = temp;
+    }
+    return arr;
+}
+// REVERSE
+// function selectionSort(arr) {
+//     const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2]]= [arr[idx2], arr[idx1]]);
+//     for (let i = 0; i < arr.length; i++) {
+//         let lowest = i;
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[lowest] < arr[j]) lowest = j
+//         }
+//         if (i !== lowest) swap(arr, i, lowest)
+//     }
+//     return arr;
+// }
+console.log(selectionSort([64,23,14,35,75,54,3]));
