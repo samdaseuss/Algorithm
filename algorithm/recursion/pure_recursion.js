67d1cbfdbff7e24a61e6648ea92c782afdefbaa7
yellpall <ohj3423@naver.com>
2023-04-07 05:00:58 +0900

    Add pure recursion

---
+++ algorithm/recursion/pure_recursion.js
@@ -0,0 +1,9 @@
+function collectOddValues(arr) {
+    let newArr = [];
+    if(arr.length === 0) return newArr;
+    if(arr[0] % 2 !== 0) newArr.push(arr[0]);
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+const array = collectOddValues([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+console.log(array);
