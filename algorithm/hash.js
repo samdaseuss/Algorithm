46966bfc460c066774e57821d4894c2ccc7f5640
yellpall <ohj3423@naver.com>
2023-04-20 20:32:54 +0900

    Add improved hash

--- algorithm/hash.js
+++ algorithm/hash.js
@@ -6,4 +6,16 @@ function hash(key, arrayLen) {
       total = (total + value) % arrayLen;
     }
     return total;
-  }
+}
+
+// improved hash
+function hash(key, arrayLen) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96
+      total = (total * WEIRD_PRIME + value) % arrayLen;
+    }
+    return total;
+}
