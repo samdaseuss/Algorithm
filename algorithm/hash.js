fdf5587999cc0b3d5e0902a2734b8fc846a3f3f3
yellpall <ohj3423@naver.com>
2023-04-25 01:18:42 +0900

    Add value method

--- algorithm/hash.js
+++ algorithm/hash.js
@@ -66,6 +66,19 @@ class HashTable {
         }
         return keysArr;
       }
+      values(){
+        let valuesArr = [];
+        for(let i = 0; i < this.keyMap.length; i++){
+          if(this.keyMap[i]){
+            for(let j = 0; j < this.keyMap[i].length; j++){
+              if(!valuesArr.includes(this.keyMap[i][j][1])){
+                valuesArr.push(this.keyMap[i][j][1])
+              }
+            }
+          }
+        }
+        return valuesArr;
+      }
   }
   
   let ht = new HashTable(17);
