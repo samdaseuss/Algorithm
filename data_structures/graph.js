4c5c29edbe3466a667107b3737e76bc9c49b8a1b
yellpall <ohj3423@naver.com>
2023-05-03 13:21:02 +0900

    Update graph

--- data_structures/graph.js
+++ data_structures/graph.js
@@ -2,13 +2,16 @@ class Graph{
     constructor(){
         this.adjacencyList = {};
     }
+    
     addVertex(vertex){
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+
     addEdge(v1,v2){
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
     }
+
     removeEdge(vertex1,vertex2){
         this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
             v => v !== vertex2
@@ -17,6 +20,7 @@ class Graph{
             v => v !== vertex1
         );
     }
+
     removeVertex(vertex){
         while(this.adjacencyList[vertex].length){
             const adjacentVertex = this.adjacencyList[vertex].pop();
