6d138ebacbbeafebaef1ab3179e9b0dcb4e1352b
yellpall <ohj3423@naver.com>
2023-11-09 19:25:29 +0900

    Hash Function

--- hash_table/hash_function.js
+++ hash_table/hash_function.js
@@ -6,11 +6,29 @@
  * @return {number} 해쉬된 값
  */
 
-function hash(key, arrayLen) {
+function hash_ver1(key, arrayLen) {
   let total = 0;
   for (let char of key) {
     let value = char.charCodeAt(0) - 96
     total = (total + value) % arrayLen;
+  }
+  return total;
+}
+
+
+/**
+ * 성능이 개선된 해쉬 함수
+ * @param {string} key 키 값
+ * @param {number} arrayLen 해쉬 길이 
+ * @returns {number} 해쉬된 값
+ */
+function hash_ver2(key, arrayLen) {
+  let total = 0;
+  let PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total * PRIME + value) % arrayLen;
   }
   return total;
 }
