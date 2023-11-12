5decf10cdab3b77b3047680e9460fa70326c5f3f
yellpall <ohj3423@naver.com>
2023-11-12 12:48:13 +0900

    의사 코드 작성

--- hash_table/Hash.js
+++ hash_table/Hash.js
@@ -22,7 +22,10 @@ class HashTable {
         this.keyMap[index].push([key, value]);  // 3. separate chaining를 통해 키-값 쌍을 해쉬 테이블 배열에 저장
     }
 
-    get(key) {
+    get(key) { // 1. 키를 입력 받음
+        // 2. 키를 해쉬 처리함
+        // 3. 키-값 쌍을 찾은 다음에 출력
+        // 4.찾을 수 없다면 언디파인드 출력 
     }
 }
 
