1f563529c6556684829dd5e060c86f9ee9098337
yellpall <ohj3423@naver.com>
2023-11-27 17:49:36 +0900

    1920 수정

--- BaekjoonOJ/1920.js
+++ BaekjoonOJ/1920.js
@@ -1,5 +1,6 @@
 import { createInterface } from "readline";
 
+
 const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -9,12 +10,15 @@ const rl = createInterface({
 let input = [];
 
 /**
- * [ 문제 ]
+ * $ 문제 | 백준 1920번
+ * 
  *  N개의 정수 A[1], A[2], …, A[N]이 주어져 있을
  *  때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그
  *  램을 작성하시오.
  * 
- * [ 입력 ]
+ * 
+ * $ 입력
+ * 
  * 첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다.
  * 다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 
  * 주어진다. 다음 줄에는 M(1 ≤ M ≤ 100,000)이 주
@@ -22,12 +26,15 @@ let input = [];
  * 이 A안에 존재하는지 알아내면 된다. 모든 정수의 범위는
  *  -231 보다 크거나 같고 231보다 작다.
  * 
- * [ 출력 ]
+ * $ 출력 
+ * 
  * M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않
  * 으면 0을 출력한다.
+ * 
  */
 
 let A = ['1','5','6','3','2'];
+
 
 function getString(str) {
 
@@ -80,6 +87,7 @@ function getString(str) {
 
     return '0';
 }
+
 
 rl.on( 
         "line",
