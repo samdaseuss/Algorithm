66bf3ccad26ed1665182a132a4ad805ecf46e096
samdaseuss <ohj3423@naver.com>
2024-03-12 19:25:16 +0900

    feat: recursive + memorization

--- algorithm/Dynamic Programming/memo-ized.js
+++ algorithm/Dynamic Programming/memo-ized.js
@@ -1,13 +1,21 @@
 // Recursion + Memorization
-function fib(n, memo=[]) {
+
+// function fib(n, memo=[]) {
+//     if(memo[n] !== undefined) return memo[n];
+//     if(n <= 2) return 1;
+//     var res = fib(n-1, memo) + fib(n-2, memo);
+//     memo[n] = res;
+//     return res;
+// }
+
+
+function fib(n, memo=[undefined,1,1]) {
     if(memo[n] !== undefined) return memo[n];
-    if(n <= 2) return 1;
-
-    var res = fib(n-1, memo) + fib(n-2, memo);
+    var res = fib(n-1, memo) + fib(n-2, memo)
     memo[n] = res;
-
-    return res;
+    return res
 }
+
 
 const solution = fib(5);
 
